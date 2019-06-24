38b99a48885e2890bd67109df7bbd5c3c92d612b
jdorhauer <jdorhauer@yahoo.com>
2019-06-24 10:29:34 -0400

    Added Restaurant entity and DbContext

---
+++ ResaurantRaterMVC/Models/Restaurant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ResaurantRaterMVC.Models
+{
+    public class Restaurant
+    {
+        public int RestaurantID { get; set; }
+        public string Name { get; set; }
+        public string FoodType { get; set; }
+        public double Rating { get; set; }
+    }
+
+    public class RestaurantDbContent : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
