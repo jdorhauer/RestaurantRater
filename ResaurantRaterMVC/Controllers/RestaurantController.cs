1aef72c3a514f0a543b10d04c598cfe0c999320a
jdorhauer <jdorhauer@yahoo.com>
2019-06-24 16:36:53 -0400

    Added Edit Controller Method

--- ResaurantRaterMVC/Controllers/RestaurantController.cs
+++ ResaurantRaterMVC/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRaterMVC.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -39,6 +40,35 @@ namespace RestaurantRaterMVC.Controllers
             return View(restaurant);
         }
 
+        // GET: Restaurant/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+        // POST: Restaurant/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(restaurant).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
+
         // GET: Restaurant/Delete/{id}
         public ActionResult Delete(int? id)
         {
@@ -56,9 +86,9 @@ namespace RestaurantRaterMVC.Controllers
 
         //POST: Restaurant/Delete/{id}
         [HttpPost]
-        [ActionName("Delete")]
+        //[ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        public ActionResult Delete(int id)
         {
             Restaurant restaurant = _db.Restaurants.Find(id);
             _db.Restaurants.Remove(restaurant);
