f1bd12b2ba070acefa2280128f14a44e9e421d54
jdorhauer <jdorhauer@yahoo.com>
2019-06-24 09:57:10 -0400

    Add project files.

---
+++ ResaurantRaterMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ResaurantRaterMVC.Startup))]
+namespace ResaurantRaterMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
